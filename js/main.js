9d8fc9991c9997e2ee09830b159be0f8f96bb2c6
UserYulia <yulka_galkina@mail.ru>
2020-10-02 13:48:23 +0300

    create modal window and make it responsive

--- js/main.js
+++ js/main.js
@@ -1,43 +1,62 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-    // Optional parameters
-    loop: true,
+$(document).ready(function() {
+    var hotelSlider = new Swiper('.hotel-slider', {
+        // Optional parameters
+        loop: true,
+    
+        // Navigation arrows
+        navigation: {
+          nextEl: '.slider-button--next',
+          prevEl: '.slider-button--prev',
+        }
+    });
+    
+    var reviewsSlider = new Swiper('.reviews-slider', {
+        // Optional parameters
+        loop: true,
+    
+        // Navigation arrows
+        navigation: {
+          nextEl: '.reviews-slider__button--next',
+          prevEl: '.reviews-slider__button--prev',
+        }
+    });
+    
+    document.addEventListener('keydown', onKeydown);
+    
+    $('.newsletter').parallax({imageSrc: './img/newsletter-bg.jpg', speed: 0.2});
+    
+    var menuButton = document.querySelector('.menu-btn');
+    menuButton.addEventListener('click', function () {
+        document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
+    });
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider-button--next',
-      prevEl: '.slider-button--prev',
-    }
+    var modalButton = $('[data-toggle=modal]');
+    var closeButton = $('.modal__close');
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog= $('.modal__dialog');
+    modalButton.on('click', openModal);
+    closeButton.on('click', closeModal);
+
+    function openModal() {
+        modalOverlay.addClass('modal__overlay--visible');
+        modalDialog.addClass('modal__dialog--visible')
+    };
+
+    function closeModal(event) {
+        event.preventDefault();
+        modalOverlay.removeClass('modal__overlay--visible');
+        modalDialog.removeClass('modal__dialog--visible')
+    };
+
+    function onKeydown(e) {
+        if (e.key == "ArrowLeft") {
+            hotelSlider.slidePrev();
+        }
+        if (e.key == "ArrowRight") {
+            hotelSlider.slideNext();
+        }
+        if (e.key == "Escape") {
+            closeModal(e);
+        }
+    };
 });
-
-var reviewsSlider = new Swiper('.reviews-slider', {
-    // Optional parameters
-    loop: true,
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.reviews-slider__button--next',
-      prevEl: '.reviews-slider__button--prev',
-    }
-});
-
-document.addEventListener('keydown', swipe);
-
-function swipe(e) {
-    if (e.key == "ArrowLeft") {
-        hotelSlider.slidePrev();
-    }
-    if (e.key == "ArrowRight") {
-        hotelSlider.slideNext();
-    }
-};
-
-$('.newsletter').parallax({imageSrc: './img/newsletter-bg.jpg', speed: 0.2});
-
-var menuButton = document.querySelector('.menu-btn');
-menuButton.addEventListener('click', function () {
-    document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
-});
-
-
-  
-
---
+++ send.php
@@ -0,0 +1,64 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+if ($email == null) {
+    $title = "Обращение Best tour plan";
+    $body = "
+    <h2>Новое обращение</h2>
+    <b>Имя:</b> $name<br>
+    <b>Телефон:</b> $phone<br><br>
+    <b>Сообщение:</b><br>$message
+    ";
+} else {
+    $title = "Подписка Best tour plan";
+    $body = "
+    <h2>Новая подписка</h2>
+    <b>Email:</b> $email
+    ";
+}
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'jullia.halkina@gmail.com'; // Логин на почте
+    $mail->Password   = '303931Aa'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('jullia.halkina@gmail.com', 'Юлия Галкина'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('halkina18@gmail.com'); // Ещё один, если нужен
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thanks.html');
